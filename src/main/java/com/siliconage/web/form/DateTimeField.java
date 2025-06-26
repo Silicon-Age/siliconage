5bd27efa09482148d135ab19b915c5d182aef175
Jonah Greenthal <jonah@naqt.com>
2025-06-26 08:30:48 -0500

    Implement step method; implement min/max methods that use LocalDate

--- src/main/java/com/siliconage/web/form/DateTimeField.java
+++ src/main/java/com/siliconage/web/form/DateTimeField.java
@@ -1,17 +1,25 @@
 package com.siliconage.web.form;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeField<T extends DateTimeField<?>> extends HTMLInputField<T, LocalDateTime> {
-	public static final DateTimeFormatter WIRE_FORMAT = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+	// The lack of milliseconds may cause a value to lose precision when it round-trips through a field.
+	// For our purposes, I think that's okay, but it's not ideal.
+	public static final DateTimeFormatter WIRE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 	
 	public DateTimeField(String argName, LocalDateTime argSavedValue, FormValueProvider argEnteredValueProvider) {
 		super(argName, argSavedValue, argEnteredValueProvider);
+		
+		step(1);
 	}
 	
 	public DateTimeField(String argName, LocalDateTime argValue) {
 		super(argName, argValue);
+		
+		step(1);
 	}
 	
 	public DateTimeField(String argName) {
@@ -48,11 +56,29 @@ public class DateTimeField<T extends DateTimeField<?>> extends HTMLInputField<T,
 		return castThis();
 	}
 	
+	public T min(LocalDate argMin) {
+		return min(argMin == null ? null : argMin.atStartOfDay());
+	}
+	
 	public T max(LocalDateTime argMax) {
 		if (argMax == null) {
 			removeAttribute("max");
 		} else {
 			attribute("max", argMax.format(WIRE_FORMAT));
+		}
+		
+		return castThis();
+	}
+	
+	public T max(LocalDate argMax) {
+		return max(argMax == null ? null : argMax.atTime(LocalTime.MAX));
+	}
+	
+	public T step(Integer argStepSeconds) {
+		if (argStepSeconds == null) {
+			removeAttribute("step");
+		} else {
+			attribute("step", String.valueOf(argStepSeconds));
 		}
 		
 		return castThis();
